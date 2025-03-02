3d2e4ecb4770043da92da34c00206e7d6cd90489
Mohammed-Ali2002 <mohammadaliaw2002@gmail.com>
2025-03-02 14:55:21 -0500

    Add files via upload
    "Initial commit of hello.py".

--- hello.py
+++ hello.py
@@ -1,7 +1,2 @@
 #hello.py
-try:
- courseNum = eval(input("Enter your course number: "))
- print(f"Hello, welcome to the course ET{courseNum}!")
-except:
- print("Hello, world!")
-
+print("Hello, World!")
